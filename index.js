76b0f5d5d64c646901fbecf4600f55ae5c6f512f
Fang Jin <windmaomao@gmail.com>
2013-07-29 18:27:28 -0400

    install module express and socket

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-require('colors');
-console.log('smashing node'.rainbow);
+var express = require('express'),
+  sio = require('socket.io');
+
+var app = express()
+, server = require('http').createServer(app);
+
+app.use(express.static('public'));
+server.listen(3000);
+
+var io = sio.listen(server);
+io.sockets.on('connection', function(socket) {
+  // console.log('someone connected');
+  socket.on('join', function(name) {
+    socket.nickname = name;
+    // emit to that client
+    // socket.emit('news', name + ' joined the chat.');
+    // emit to all clients
+    // io.sockets.emit('news', name + ' joined the chat.');
+    // emit to all except this client
+    socket.broadcast.emit('news', name + ' joined the chat.');
+  });
+  socket.on('text', function(msg) {
+    // io.sockets.emit('news', msg);
+    socket.broadcast.emit('text', socket.nickname, msg);
+  })
+});
+
